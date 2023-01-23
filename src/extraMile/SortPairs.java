d009da496498d9fa5357f3e4408ec703bb6bb8ac
raduandreipopescu <popescu.a.radu@gmail.com>
2023-01-23 13:18:53 +0200

    Course 7 Homework - complete (improved SortPairs from extra mile)

--- src/extraMile/SortPairs.java
+++ src/extraMile/SortPairs.java
@@ -2,7 +2,7 @@ package extraMile;
 
 public class SortPairs {
     public static void main(String[] args) {
-        int[] numbers = {8, 7, 2, 5, 3, 1};
+        int[] numbers = {8, 7, 2, 5, 3, 1, 7, 7};
         int target = 10;
         for (int i = 0; i < numbers.length; i++) {
             for (int j = i; j < numbers.length; j++) {
